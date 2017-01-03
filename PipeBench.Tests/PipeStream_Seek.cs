c6b34dfebe652acf8dd5092002277d42c6d5caf5
Gert Driesen <gert.driesen@telenet.be>
2017-01-03 06:34:17 +0100

    Added some tests for Seek.

---
+++ PipeBench.Tests/PipeStream_Seek.cs
@@ -0,0 +1,442 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Renci.SshNet.Common;
+using System;
+using System.IO;
+
+namespace PipeBench.Tests
+{
+    [TestClass]
+    public class PipeStream_Seek
+    {
+        [TestMethod]
+        public void SeekOriginBegin_OffsetNegative_PositionedAtBeginOfStream_ShouldThrowArgumentOutOfRangeException()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+
+            try
+            {
+                pipeStream.Seek(-1L, SeekOrigin.Begin);
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Assert.IsNull(ex.InnerException);
+                Assert.AreEqual(string.Format("Cannot be less than the current position.{0}Parameter name: {1}", Environment.NewLine, ex.ParamName), ex.Message);
+                Assert.AreEqual("offset", ex.ParamName);
+            }
+        }
+
+        [TestMethod]
+        public void SeekOriginBegin_OffsetNegative_PositionedAtMiddleOfStream_ShouldThrowArgumentOutOfRangeException()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+            pipeStream.Read(data, 0, 2);
+
+            try
+            {
+                pipeStream.Seek(-1L, SeekOrigin.Begin);
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Assert.IsNull(ex.InnerException);
+                Assert.AreEqual(string.Format("Cannot be less than the current position.{0}Parameter name: {1}", Environment.NewLine, ex.ParamName), ex.Message);
+                Assert.AreEqual("offset", ex.ParamName);
+            }
+        }
+
+        [TestMethod]
+        public void SeekOriginBegin_OffsetNegative_PositionedAtEndOfStream_ShouldThrowArgumentOutOfRangeException()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+            pipeStream.Read(data, 0, 4);
+
+            try
+            {
+                pipeStream.Seek(-1L, SeekOrigin.Begin);
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Assert.IsNull(ex.InnerException);
+                Assert.AreEqual(string.Format("Cannot be less than the current position.{0}Parameter name: {1}", Environment.NewLine, ex.ParamName), ex.Message);
+                Assert.AreEqual("offset", ex.ParamName);
+            }
+        }
+
+        [TestMethod]
+        public void SeekOriginBegin_OffsetLessThanPosition_ShouldThrowArgumentOutOfRangeException()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+            pipeStream.Read(data, 0, 2);
+
+            try
+            {
+                pipeStream.Seek(1L, SeekOrigin.Begin);
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Assert.IsNull(ex.InnerException);
+                Assert.AreEqual(string.Format("Cannot be less than the current position.{0}Parameter name: {1}", Environment.NewLine, ex.ParamName), ex.Message);
+                Assert.AreEqual("offset", ex.ParamName);
+            }
+        }
+
+        [TestMethod]
+        public void SeekOriginBegin_OffsetGreaterThanLength_ShouldThrowArgumentOutOfRangeException()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+
+            try
+            {
+                pipeStream.Seek(data.Length + 1, SeekOrigin.Begin);
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Assert.IsNull(ex.InnerException);
+                Assert.AreEqual(string.Format("Cannot seek beyond the end of the stream.{0}Parameter name: {1}", Environment.NewLine, ex.ParamName), ex.Message);
+                Assert.AreEqual("offset", ex.ParamName);
+            }
+        }
+
+        [TestMethod]
+        public void SeekOriginBegin_OffsetEqualsPosition_PositionedAtBeginOfStream_ShouldNotMovePosition()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+
+            var newPosition = pipeStream.Seek(0L, SeekOrigin.Begin);
+
+            Assert.AreEqual(0L, newPosition);
+            Assert.AreEqual(0L, pipeStream.Position);
+
+            var buffer = new byte[2];
+            var bytesRead = pipeStream.Read(buffer, 0, buffer.Length);
+            Assert.AreEqual(2, bytesRead);
+            Assert.AreEqual(1, buffer[0]);
+            Assert.AreEqual(4, buffer[1]);
+        }
+
+        [TestMethod]
+        public void SeekOriginBegin_OffsetEqualsPosition_PositionedInMiddleOfStream_ShouldNotMovePosition()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+            pipeStream.Read(data, 0, 2);
+
+            var newPosition = pipeStream.Seek(0L, SeekOrigin.Begin);
+
+            Assert.AreEqual(2L, newPosition);
+            Assert.AreEqual(2L, pipeStream.Position);
+
+            var buffer = new byte[2];
+            var bytesRead = pipeStream.Read(buffer, 0, buffer.Length);
+            Assert.AreEqual(2, bytesRead);
+            Assert.AreEqual(3, buffer[0]);
+            Assert.AreEqual(7, buffer[1]);
+        }
+
+        [TestMethod]
+        public void SeekOriginBegin_OffsetEqualsPosition_PositionedAtEndOfStream_ShouldNotMovePosition()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+            pipeStream.Read(data, 0, 4);
+
+            var newPosition = pipeStream.Seek(data.Length, SeekOrigin.Begin);
+
+            Assert.AreEqual(4L, newPosition);
+            Assert.AreEqual(4L, pipeStream.Position);
+
+            // allow detect EOS
+            pipeStream.Dispose();
+
+            var buffer = new byte[2];
+            var bytesRead = pipeStream.Read(buffer, 0, buffer.Length);
+            Assert.AreEqual(0, bytesRead);
+            Assert.AreEqual(0, buffer[0]);
+            Assert.AreEqual(0, buffer[1]);
+        }
+
+        [TestMethod]
+        public void SeekOriginBegin_OffsetGreaterThanPositionAndLessThanLength_PositionedAtBeginOfStream()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+
+            var newPosition = pipeStream.Seek(3L, SeekOrigin.Begin);
+
+            Assert.AreEqual(3L, newPosition);
+            Assert.AreEqual(3L, pipeStream.Position);
+
+            var buffer = new byte[1];
+            var bytesRead = pipeStream.Read(buffer, 0, buffer.Length);
+            Assert.AreEqual(1, bytesRead);
+            Assert.AreEqual(7, buffer[0]);
+        }
+
+        [TestMethod]
+        public void SeekOriginBegin_OffsetGreaterThanPositionAndEqualToLength_PositionedAtBeginOfStream()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+
+            var newPosition = pipeStream.Seek(4L, SeekOrigin.Begin);
+
+            Assert.AreEqual(4L, newPosition);
+            Assert.AreEqual(4L, pipeStream.Position);
+
+            // allow detect EOS
+            pipeStream.Dispose();
+
+            var buffer = new byte[2];
+            var bytesRead = pipeStream.Read(buffer, 0, buffer.Length);
+            Assert.AreEqual(0, bytesRead);
+            Assert.AreEqual(0, buffer[0]);
+            Assert.AreEqual(0, buffer[1]);
+        }
+
+        [TestMethod]
+        public void SeekOriginBegin_OffsetGreaterThanPositionAndLessThanLength_PositionedInMiddleOfStream()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+            pipeStream.Read(data, 0, 2);
+
+            var newPosition = pipeStream.Seek(3L, SeekOrigin.Begin);
+
+            Assert.AreEqual(3L, newPosition);
+            Assert.AreEqual(3L, pipeStream.Position);
+
+            var buffer = new byte[1];
+            var bytesRead = pipeStream.Read(buffer, 0, buffer.Length);
+            Assert.AreEqual(1, bytesRead);
+            Assert.AreEqual(7, buffer[0]);
+        }
+
+        [TestMethod]
+        public void SeekOriginBegin_OffsetGreaterThanPositionAndEqualToLength_PositionedInMiddleOfStream()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+            pipeStream.Read(data, 0, 2);
+
+            var newPosition = pipeStream.Seek(4L, SeekOrigin.Begin);
+
+            Assert.AreEqual(4L, newPosition);
+            Assert.AreEqual(4L, pipeStream.Position);
+
+            // allow detect EOS
+            pipeStream.Dispose();
+
+            var buffer = new byte[2];
+            var bytesRead = pipeStream.Read(buffer, 0, buffer.Length);
+            Assert.AreEqual(0, bytesRead);
+            Assert.AreEqual(0, buffer[0]);
+            Assert.AreEqual(0, buffer[1]);
+        }
+
+        [TestMethod]
+        public void SeekOriginCurrent_OffsetNegative_PositionedAtBeginOfStream_ShouldThrowArgumentOutOfRangeException()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+
+            try
+            {
+                pipeStream.Seek(-1L, SeekOrigin.Current);
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Assert.IsNull(ex.InnerException);
+                Assert.AreEqual(string.Format("Cannot move backward.{0}Parameter name: {1}", Environment.NewLine, ex.ParamName), ex.Message);
+                Assert.AreEqual("offset", ex.ParamName);
+            }
+        }
+
+        [TestMethod]
+        public void SeekOriginCurrent_OffsetNegative_PositionedInMiddleOfStream_ShouldThrowArgumentOutOfRangeException()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+            pipeStream.Read(data, 0, 2);
+
+            try
+            {
+                pipeStream.Seek(-1L, SeekOrigin.Current);
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Assert.IsNull(ex.InnerException);
+                Assert.AreEqual(string.Format("Cannot move backward.{0}Parameter name: {1}", Environment.NewLine, ex.ParamName), ex.Message);
+                Assert.AreEqual("offset", ex.ParamName);
+            }
+        }
+
+        [TestMethod]
+        public void SeekOriginCurrent_OffsetNegative_PositionedAtEndOfStream_ShouldThrowArgumentOutOfRangeException()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+            pipeStream.Read(data, 0, 4);
+
+            try
+            {
+                pipeStream.Seek(-1L, SeekOrigin.Current);
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Assert.IsNull(ex.InnerException);
+                Assert.AreEqual(string.Format("Cannot move backward.{0}Parameter name: {1}", Environment.NewLine, ex.ParamName), ex.Message);
+                Assert.AreEqual("offset", ex.ParamName);
+            }
+        }
+
+        [TestMethod]
+        public void SeekOriginCurrent_OffsetZero_PositionedAtBeginOfStream_ShouldNotMovePosition()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+
+            var newPosition = pipeStream.Seek(0L, SeekOrigin.Current);
+
+            Assert.AreEqual(0L, newPosition);
+            Assert.AreEqual(0L, pipeStream.Position);
+
+            var buffer = new byte[2];
+            var bytesRead = pipeStream.Read(buffer, 0, buffer.Length);
+            Assert.AreEqual(2, bytesRead);
+            Assert.AreEqual(1, buffer[0]);
+            Assert.AreEqual(4, buffer[1]);
+        }
+
+        [TestMethod]
+        public void SeekOriginCurrent_OffsetZero_PositionedAtMiddleOfStream_ShouldNotMovePosition()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+            pipeStream.Read(data, 0, 2);
+
+            var newPosition = pipeStream.Seek(0L, SeekOrigin.Current);
+
+            Assert.AreEqual(2L, newPosition);
+            Assert.AreEqual(2L, pipeStream.Position);
+
+            var buffer = new byte[2];
+            var bytesRead = pipeStream.Read(buffer, 0, buffer.Length);
+            Assert.AreEqual(2, bytesRead);
+            Assert.AreEqual(3, buffer[0]);
+            Assert.AreEqual(7, buffer[1]);
+        }
+
+        [TestMethod]
+        public void SeekOriginCurrent_OffsetZero_PositionedAtEndOfStream_ShouldNotMovePosition()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+            pipeStream.Read(data, 0, 4);
+
+            pipeStream.Seek(0L, SeekOrigin.Current);
+
+            // allow detect EOS
+            pipeStream.Dispose();
+
+            var buffer = new byte[2];
+            var bytesRead = pipeStream.Read(buffer, 0, buffer.Length);
+            Assert.AreEqual(0, bytesRead);
+            Assert.AreEqual(0, buffer[0]);
+            Assert.AreEqual(0, buffer[1]);
+        }
+
+        [TestMethod]
+        public void SeekOriginCurrent_OffsetPlusPositionAtMiddleOfStream()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+            pipeStream.Read(data, 0, 1);
+
+            var newPosition = pipeStream.Seek(2L, SeekOrigin.Current);
+
+            Assert.AreEqual(3L, newPosition);
+            Assert.AreEqual(3L, pipeStream.Position);
+
+            var buffer = new byte[1];
+            var bytesRead = pipeStream.Read(buffer, 0, buffer.Length);
+            Assert.AreEqual(1, bytesRead);
+            Assert.AreEqual(7, buffer[0]);
+        }
+
+        [TestMethod]
+        public void SeekOriginCurrent_OffsetPlusPositionAtEndOfStream()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+            pipeStream.Read(data, 0, 2);
+
+            var newPosition = pipeStream.Seek(data.Length - pipeStream.Position, SeekOrigin.Current);
+
+            Assert.AreEqual(4, newPosition);
+            Assert.AreEqual(4, pipeStream.Position);
+
+            // allow detect EOS
+            pipeStream.Dispose();
+
+            var buffer = new byte[2];
+            var bytesRead = pipeStream.Read(buffer, 0, buffer.Length);
+            Assert.AreEqual(0, bytesRead);
+            Assert.AreEqual(0, buffer[0]);
+            Assert.AreEqual(0, buffer[1]);
+        }
+
+        [TestMethod]
+        public void SeekOriginCurrent_OffsetPlusPositionBeyondEndOfStream_ShouldThrowArgumentOutOfRangeException()
+        {
+            var data = new byte[] { 1, 4, 3, 7 };
+            var pipeStream = new LinkedListPipeStream();
+            pipeStream.Write(data, 0, data.Length);
+            pipeStream.Read(data, 0, 2);
+
+            try
+            {
+                pipeStream.Seek(data.Length - pipeStream.Position + 1, SeekOrigin.Current);
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Assert.IsNull(ex.InnerException);
+                Assert.AreEqual(string.Format("Cannot seek beyond the end of the stream.{0}Parameter name: {1}", Environment.NewLine, ex.ParamName), ex.Message);
+                Assert.AreEqual("offset", ex.ParamName);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -57,10 +57,207 @@ namespace PipeBench
                 iterations = Int64.Parse(args[0]);
             }
 
+            SeekBeginTest(new LinkedListPipeStream());
+            SeekCurrentTest(new LinkedListPipeStream());
+            SetLengthTest1(new LinkedListPipeStream());
+
             Test(new BlockingPipeStream());
             Test(new BufferingPipeStream());
             Test(new CompatBufferingPipeStream());
             Test(new LinkedListPipeStream());
+        }
+
+        private static void SetLengthTest1(Stream stream)
+        {
+            stream.Write(new byte[] { 0x05, 0x02, 0x04, 0x01, 0x00, 0x06 }, 1, 4);
+            stream.Write(new byte[] { 0x03, 0x01, 0x08, 0x0e, 0x0c, 0x03 }, 2, 3);
+
+            var buffer = new byte[1];
+            stream.Read(buffer, 0, 1);
+
+            stream.SetLength(3);
+
+            if (stream.Length != 3)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+
+            if (stream.Position != 1)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+
+            stream.Dispose();
+            buffer = new byte[6];
+            var bytesRead = stream.Read(buffer, 0, 6);
+
+            if (bytesRead != 2)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+
+            if (buffer[0] != 0x04)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+            if (buffer[1] != 0x01)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+            if (buffer[2] != 0)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+            if (buffer[3] != 0)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+            if (buffer[4] != 0)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+            if (buffer[5] != 0)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+        }
+
+        private static void SeekCurrentTest(Stream stream)
+        {
+            stream.Write(new byte[] { 0x05, 0x02, 0x04, 0x01, 0x00, 0x06 }, 1, 4);
+            stream.Write(new byte[] { 0x03, 0x01, 0x08, 0x0e, 0x0c, 0x03 }, 2, 3);
+
+            var buffer = new byte[6];
+            var bytesRead = stream.Read(buffer, 3, 2);
+            if (bytesRead != 2)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+
+            if (buffer[0] != 0)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+            if (buffer[1] != 0)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+            if (buffer[2] != 0)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+            if (buffer[3] != 0x02)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+            if (buffer[4] != 0x04)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+            if (buffer[5] != 0)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+
+            var newPosition = stream.Seek(3, SeekOrigin.Current);
+            Console.WriteLine("new position=" + newPosition);
+
+            buffer = new byte[4];
+            bytesRead = stream.Read(buffer, 0, 2);
+            if (bytesRead != 2)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+
+            if (buffer[0] != 0x0e)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+            if (buffer[1] != 0x0c)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+            if (buffer[2] != 0)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+            if (buffer[3] != 0)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+        }
+
+        private static void SeekBeginTest(Stream stream)
+        {
+            stream.Write(new byte[] { 0x05, 0x02, 0x04, 0x01, 0x00, 0x06 }, 1, 4);
+            stream.Write(new byte[] { 0x03, 0x01, 0x08, 0x0e, 0x0c, 0x03 }, 2, 3);
+
+            var buffer = new byte[6];
+            var bytesRead = stream.Read(buffer, 3, 2);
+            if (bytesRead != 2)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+
+            var newPosition = stream.Seek(3, SeekOrigin.Begin);
+            if (newPosition != 3)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+
+            if (stream.Position != 3)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+
+            buffer = new byte[4];
+            bytesRead = stream.Read(buffer, 0, 2);
+            if (bytesRead != 2)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+
+            if (buffer[0] != 0x00)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+            if (buffer[1] != 0x08)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
+
+            if (stream.Position != 5)
+            {
+                Console.WriteLine("Failure");
+                return;
+            }
         }
     }
 }
